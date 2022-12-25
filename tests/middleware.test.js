b2a808f6bb33350373d02809e01515bb739192f9
Ernestas Ragainis <ernestas.ragainis@gmail.com>
2022-12-25 20:05:06 +0200

    chore: fixed dotenv

--- tests/middleware.test.js
+++ tests/middleware.test.js
@@ -1,16 +1,16 @@
-const { isEmailValid, passwordValid, hasUpperCase } = require("../utils/main");
+const { isEmailValid, passwordValid, hasUpperCase } = require('../utils/main');
 
-test("checks if email has @ symbol and checks email length", () => {
-  const result = isEmailValid("tas@gmail.com");
+test('checks if email has @ symbol and checks email length', () => {
+  const result = isEmailValid('tas@gmail.com');
   expect(result).toBe(true);
 });
 
-test("validates user password", () => {
-  const result = passwordValid("password1", "password1");
-  expect(result).toBe("yes");
+test('validates user password', () => {
+  const result = passwordValid('password1', 'password1');
+  expect(result).toBe('yes');
 });
 
-test("expects atleast one uppercase letter", () => {
-  const result = hasUpperCase("svEikas");
+test('expects atleast one uppercase letter', () => {
+  const result = hasUpperCase('svEikas');
   expect(result).toBe(true);
 });
--- utils/main.js
+++ utils/main.js
@@ -1,16 +1,12 @@
-const validator = require("email-validator");
+const validator = require('email-validator');
 
 module.exports = {
-  isEmailValid: (email) => {
-    return validator.validate(email);
-  },
+  isEmailValid: (email) => validator.validate(email),
   passwordValid: (pass1, pass2) => {
-    if (pass1 !== pass2) return "no";
-    if (pass1.length < 5 || pass2.length < 5) return "no";
-    if (pass1.length > 100 || pass2.length > 100) return "no";
-    return "yes";
+    if (pass1 !== pass2) return 'no';
+    if (pass1.length < 5 || pass2.length < 5) return 'no';
+    if (pass1.length > 100 || pass2.length > 100) return 'no';
+    return 'yes';
   },
-  hasUpperCase: (str) => {
-    return str !== str.toLowerCase();
-  },
+  hasUpperCase: (str) => str !== str.toLowerCase(),
 };
